c7e7692fffb71714b7deb0b8d2a9b77cc99d488d
王文博 <wangwenbo@wangwenbodeMacBook-Pro.local>
2017-07-16 12:04:42 +0800

    first commit

---
+++ WBShuffling/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WBShuffling
+//
+//  Created by 王文博 on 2017/7/16.
+//  Copyright © 2017年 王文博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WBShuffling/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WBShuffling
+//
+//  Created by 王文博 on 2017/7/16.
+//  Copyright © 2017年 王文博. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
