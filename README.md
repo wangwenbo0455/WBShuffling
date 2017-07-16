d04e4e7e46aea254083e5bffa9eba6fd06255371
王文博 <wangwenbo@wangwenbodeMacBook-Pro.local>
2017-07-16 14:25:12 +0800

    更改readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WBShuffling
-轮播图控件,自带定时器，不用考虑timer，已经做了处理，自带缓存，使用方便，
+轮播图控件,自带定时器，不用考虑timer销毁，已经做了处理，自带缓存，使用方便，样式和动画齐全，如有特殊需求可Issues
+
+![image](https://github.com/wangwenbo0455/WBShuffling/blob/master/shufflingImage.gif)
---
+++ WBShuffling/Shuffling/WBConfiguration.m
@@ -0,0 +1,13 @@
+//
+//  WBConfiguration.m
+//  WBShuffling
+//
+//  Created by 王文博 on 2017/7/16.
+//  Copyright © 2017年 王文博. All rights reserved.
+//
+
+#import "WBConfiguration.h"
+
+@implementation WBConfiguration
+
+@end
